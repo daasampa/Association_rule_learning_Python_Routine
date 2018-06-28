c30d4d4a126bd9c58b6942080ff86b99632e0f1d
David Arango Sampayo <39955504+daasampa@users.noreply.github.com>
2018-06-28 09:01:53 -0500

    Update Modules.py
    Update to call all pyspark modules requiered to: manage data by pandas data frame structure; sql functions (count, countDistinct); tab completion option; the time module to calculate time elapsed in executions and set off the loglevel.

--- Modules.py
+++ Modules.py
@@ -1,3 +1,6 @@
-import pandas as pd, numpy as np, time, rlcompleter, readline, rlcompleter, readline
+from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from itertools import combinations, compress
+import pandas as pd, numpy as np, readline, inspect, time
+readline.parse_and_bind("tab: complete")
+spark.sparkContext.setLogLevel("OFF")
